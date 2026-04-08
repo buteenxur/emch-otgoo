6a737e4eca4c9dcb72fbd1364b8976f895212b97
buteenxur <buteenxur@gmail.com>
2026-04-08 14:52:54 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse, JSONResponse
+import uvicorn, os
+
+app = FastAPI(title="Эмч Отгоо")
+
+GEMINI_KEY = os.environ.get("GEMINI_API_KEY", "")
+
+@app.get("/")
+async def root():
+    return FileResponse("static/index.html")
+
+@app.get("/api/key")
+async def get_key():
+    return JSONResponse({"key": GEMINI_KEY})
+
+@app.get("/manifest.json")
+async def manifest():
+    return FileResponse("manifest.json", media_type="application/json")
+
+@app.get("/sw.js")
+async def sw():
+    return FileResponse("sw.js", media_type="application/javascript")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
